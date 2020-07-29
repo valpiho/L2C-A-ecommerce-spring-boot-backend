50b21abf9628b0be703d3e9819b74d909a029299
Valentin Piho <val.piho@gmail.com>
2020-07-29 21:13:42 +0300

    Adding Cross Origin exception

--- src/main/java/com/pibox/ecommerce/dao/ProductCategoryRepository.java
+++ src/main/java/com/pibox/ecommerce/dao/ProductCategoryRepository.java
@@ -3,8 +3,10 @@ package com.pibox.ecommerce.dao;
 import com.pibox.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Name of JSON entry and path */product-category
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
