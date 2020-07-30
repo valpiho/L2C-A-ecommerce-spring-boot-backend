b75aaf01d9d61b44cbf47e9e7112e937334da979
Valentin Piho <val.piho@gmail.com>
2020-07-30 21:25:18 +0300

    Adding searching params for categories by ID

--- src/main/java/com/pibox/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/pibox/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,16 @@
 package com.pibox.ecommerce.dao;
 
 import com.pibox.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // SELECT * FROM product where category_id=?
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
+
 }
